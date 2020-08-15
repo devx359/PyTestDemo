4ea52d56024605ea49371127108a3808aa4a2c95
debapriyo <debapriyo86@gmail.com>
2020-08-15 13:52:15 +0530

    bat file changes

--- jenkinsConfig.bat
+++ jenkinsConfig.bat
@@ -1,5 +1,17 @@
-VER
 
-E:/PythonWorkspace/PyTestDemo/venv/Scripts/python --version
+VER
+echo "Global python version"
+C:/Users/deb/AppData/Local/Programs/Python/Python37-32/python --version
+echo "Global PIP version"
+C:/Users/deb/AppData/Local/Programs/Python/Python37-32/Scripts/pip --version
+echo "Creating venv"
+
+=
+
+::C:/Users/deb/AppData/Local/Programs/Python/Python37-32/python -m venv "C:/Program Files (x86)/Jenkins/workspace/pythonTest/venv"
+echo "importing requirements"
+"C:/Program Files (x86)/Jenkins/workspace/pythonTest/venv/Scripts/pip" install -r "C:/Program Files (x86)/Jenkins/workspace/pythonTest/requirements.txt"
+REM echo "acitvating venv"
+REM "C:/Program Files (x86)/Jenkins/workspace/pythonTest/venv/Scripts/activate"
 echo "Executing pytest"
-E:/PythonWorkspace/PyTestDemo/venv/Scripts/python -m pytest E:/PythonWorkspace/PyTestDemo/Tests/Selenium/test_YT.py
+"C:/Program Files (x86)/Jenkins/workspace/pythonTest/venv/Scripts/python" -m pytest "C:/Program Files (x86)/Jenkins/workspace/pythonTest/Tests/Selenium/test_YT.py"
