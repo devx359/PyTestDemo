a2de6b94de73eea9db41fa93c83a85248197f59c
debapriyo <debapriyo86@gmail.com>
2020-08-15 11:15:53 +0530

    added config.bat file

---
+++ jenkinsConfig.bat
@@ -0,0 +1,5 @@
+VER
+
+E:/PythonWorkspace/PyTestDemo/venv/Scripts/python --version
+echo "Executing pytest"
+E:/PythonWorkspace/PyTestDemo/venv/Scripts/python -m pytest E:/PythonWorkspace/PyTestDemo/Tests/Selenium/test_YT.py
